db7e783c50f6edc0b2cf03e793463836a0c2f846
Elim <elimviolinist@gmail.com>
2023-03-27 10:30:05 -0400

    Modularizing funcs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -22,13 +23,14 @@ const PricingMatrixURL string = "https://protokendallsq.securecafe.com/rcloadcon
 type Apartment struct {
 	ID           string    `json:"id"`
 	Unit         string    `json:"unit"`
+	IDValue      string    `json:"id_value"`
 	Bedroom      string    `json:"bedroom"`
 	SqFt         uint      `json:"sq_ft"`
 	MinRent      uint      `json:"min_rent"`
 	MaxRent      uint      `json:"max_rent"`
 	Availability time.Time `json:"availability"`
 	Floor        string    `json:"floor"`
-	Quote        uint      `json:"quote"`
+	Quote        string    `json:"quote"`
 }
 
 func main() {
@@ -62,6 +64,7 @@ func main() {
 			studioApartments = append(studioApartments, apartment)
 		}
 	}
+	log.Printf("Filtered %v studio apartments", len(studioApartments))
 	// Filter by move-in date
 	var juneJulyStudioApts []Apartment
 	for _, apartment := range studioApartments {
@@ -70,7 +73,7 @@ func main() {
 			juneJulyStudioApts = append(juneJulyStudioApts, apartment)
 		}
 	}
-	log.Printf("Filtered %v apartments", len(juneJulyStudioApts))
+	log.Printf("Filtered %v apartments by move-in date", len(juneJulyStudioApts))
 
 	// Calculate pricing (best?), html tokenizer
 	juneJulyStudioApts[0].populateQuote(MoveInDate)
@@ -91,25 +94,63 @@ func (apartment *Apartment) populateQuote(moveInDate time.Time) {
 		"UnitId":            {"30653186"},
 		"UnitAvailableDate": {"5/11/2023"},
 	}.Encode()
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	resp, respErr := http.DefaultClient.Do(req)
 	if respErr != nil {
 		log.Fatal(respErr)
 	}
-	// body, readErr := io.ReadAll(resp.Body)
 	tokenizer := html.NewTokenizer(resp.Body)
 	defer resp.Body.Close()
 	// log.Print(string(body))
-	for {
-		tokenizer.Next()
-		log.Printf("Token: %v", tokenizer.Token().String())
-		log.Printf("Matched?: %v", tokenizer.Token().String() == "<tr id=\"Pricerow11\">")
-		if tokenizer.Token().String() == "<tr id=\"Pricerow0\">" {
-			log.Printf("Token: %v", tokenizer.Token())
-			break
-		} else if tokenizer.Token().Type == html.ErrorToken {
-			log.Print("Error!")
-			log.Print("\"")
-			break
+	log.Printf("Best quote for unit %v: %v", apartment.Unit, apartment.getBestQuote(tokenizer))
+}
+
+func (apartment *Apartment) getBestQuote(tokenizer *html.Tokenizer) string {
+	quotes := make([]string, 4)
+	// minRentAdjusted := apartment.MinRent
+	quotes[3] = "$" + fmt.Sprint(apartment.MinRent) + ".00"
+	for i, rowName := range []string{"Pricerow0", "Pricerow1", "Pricerow2"} {
+		quotes[i] = getRowFirstQuote(tokenizer, rowName)
+	}
+
+	bestQuote := quotes[0]
+	for _, quote := range quotes {
+		if quote < bestQuote {
+			bestQuote = quote
 		}
 	}
+	return bestQuote
+}
+
+func getRowFirstQuote(tokenizer *html.Tokenizer, rowName string) string {
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			log.Print("Error!")
+			break
+		}
+
+		// Look for start tags
+		if tokenType == html.StartTagToken {
+			_, val, _ := tokenizer.TagAttr()
+			// See if id matches row name
+			if string(val) == rowName {
+				for {
+					tokenType := tokenizer.Next()
+					if tokenType == html.ErrorToken {
+						break
+					}
+					tagname, _ := tokenizer.TagName()
+					if string(tagname) == "label" {
+						tokenizer.Next() // Get label text
+						newQuote := string(tokenizer.Text())
+						log.Printf("%v: %v", rowName, newQuote)
+						return newQuote
+					}
+				}
+				break
+			}
+		}
+	}
+	return ""
 }
