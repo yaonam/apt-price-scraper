f55b242cdbad3ffe6e3ad9bafb0790686271b46d
Elim <elimviolinist@gmail.com>
2023-03-26 22:14:49 -0400

    Tokenizing pricing matrix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module apt-price-scraper
 
 go 1.20
+
+require golang.org/x/net v0.8.0
--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
+	"golang.org/x/net/html"
 )
+
+var MoveInDate time.Time = time.Date(2023, time.July, 1, 0, 0, 0, 0, time.Local)
+var leftLimit = time.Date(2023, time.June, 1, 0, 0, 0, 0, time.Local)
+var july = time.Date(2023, time.July, 10, 0, 0, 0, 0, time.Local)
 
 const AvailableUnitsURL string = "https://protokendallsq.com/floorplans/"
 const PricingMatrixURL string = "https://protokendallsq.securecafe.com/rcloadcontent.ashx"
@@ -20,8 +26,9 @@ type Apartment struct {
 	SqFt         uint      `json:"sq_ft"`
 	MinRent      uint      `json:"min_rent"`
 	MaxRent      uint      `json:"max_rent"`
-	Availability time.Time `json:"availability"` // "2022-06-08T00:00:00-05:00"
+	Availability time.Time `json:"availability"`
 	Floor        string    `json:"floor"`
+	Quote        uint      `json:"quote"`
 }
 
 func main() {
@@ -46,6 +53,7 @@ func main() {
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
+	log.Printf("Fetched %v apartments", len(allApartments))
 
 	// Filter by studio
 	var studioApartments []Apartment
@@ -54,19 +62,18 @@ func main() {
 			studioApartments = append(studioApartments, apartment)
 		}
 	}
-
 	// Filter by move-in date
 	var juneJulyStudioApts []Apartment
-	june := time.Date(2023, time.June, 1, 0, 0, 0, 0, time.Local)
-	july := time.Date(2023, time.July, 10, 0, 0, 0, 0, time.Local)
 	for _, apartment := range studioApartments {
 		availability := apartment.Availability
-		if availability.After(june) && availability.Before(july) {
+		if availability.After(leftLimit) && availability.Before(july) {
 			juneJulyStudioApts = append(juneJulyStudioApts, apartment)
 		}
 	}
+	log.Printf("Filtered %v apartments", len(juneJulyStudioApts))
 
 	// Calculate pricing (best?), html tokenizer
+	juneJulyStudioApts[0].populateQuote(MoveInDate)
 
 	// Show pricing (ordered?)
 	prettyApts, prettyErr := json.MarshalIndent(juneJulyStudioApts, "", "  ")
@@ -76,21 +83,33 @@ func main() {
 	log.Print(string(prettyApts))
 }
 
-// func (apartment *Apartment) populateQuote(moveInDate time.Time) {
-// 	// Get price matrix
-// 	availableUnitsData := url.Values{
-// 		"action": {"available-units"},
-// 	}
-// 	req, _ := http.NewRequest("POST", AvailableUnitsURL, strings.NewReader(availableUnitsData.Encode()))
-// 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-// 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-// 	resp, respErr := http.DefaultClient.Do(req)
-// 	if respErr != nil {
-// 		log.Fatal(respErr)
-// 	}
-// 	body, readErr := io.ReadAll(resp.Body)
-// 	if readErr != nil {
-// 		log.Println(readErr)
-// 	}
-// 	resp.Body.Close()
-// }
+func (apartment *Apartment) populateQuote(moveInDate time.Time) {
+	// Get price matrix
+	req, _ := http.NewRequest("GET", PricingMatrixURL, nil)
+	req.URL.RawQuery = url.Values{
+		"contentclass":      {"pricingmatrix"},
+		"UnitId":            {"30653186"},
+		"UnitAvailableDate": {"5/11/2023"},
+	}.Encode()
+	resp, respErr := http.DefaultClient.Do(req)
+	if respErr != nil {
+		log.Fatal(respErr)
+	}
+	// body, readErr := io.ReadAll(resp.Body)
+	tokenizer := html.NewTokenizer(resp.Body)
+	defer resp.Body.Close()
+	// log.Print(string(body))
+	for {
+		tokenizer.Next()
+		log.Printf("Token: %v", tokenizer.Token().String())
+		log.Printf("Matched?: %v", tokenizer.Token().String() == "<tr id=\"Pricerow11\">")
+		if tokenizer.Token().String() == "<tr id=\"Pricerow0\">" {
+			log.Printf("Token: %v", tokenizer.Token())
+			break
+		} else if tokenizer.Token().Type == html.ErrorToken {
+			log.Print("Error!")
+			log.Print("\"")
+			break
+		}
+	}
+}
